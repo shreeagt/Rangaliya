23c63d0fa2d44e080a64eb32db0cf7b483d50b77
RahulRawatAgt <rahulrawat.agt@gmail.com>
2023-07-27 12:40:34 +0530

    added the thankyou cosmetics

--- resources/views/Website/thankyou.blade.php
+++ resources/views/Website/thankyou.blade.php
@@ -1,6 +1,20 @@
 @include('layouts.headerrang')
 <link rel='stylesheet' id='wc-blocks-vendors-style-css' href="{{ asset('css/rangrilya/post-939.css') }}" type='text/css' media='all' />
+<style>
+  .justify-content-center{
+    justify-content: center;
+  }
 
+  .bg_thank{
+    background-image: url('/img/bg_rang.jpg');
+    border-radius: 20px;
+    /* background: #fff; */
+    box-shadow: 5px 5px 10px #888888;
+    margin:100px 0;
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+</style>
 
 <main id="main" class="page-content">
   <div id="page-939" class="post-939 page type-page status-publish hentry">
@@ -12,21 +26,22 @@
   <!--hello world-->
   <section class="elementor-section elementor-top-section elementor-element elementor-element-89c48a8 elementor-section-full_width elementor-section-height-default elementor-section-height-default" data-id="89c48a8" data-element_type="section">
     <div class="elementor-container elementor-column-gap-default">
-      <div class="elementor-column elementor-col-25 elementor-top-column elementor-element elementor-element-7c524df" data-id="7c524df" data-element_type="column">
-        <div class="elementor-widget-wrap elementor-element-populated">
+      <div class="elementor-column elementor-col-100 elementor-top-column elementor-element " data-id="7c524df" data-element_type="column">
+        <div class="elementor-widget-wrap elementor-element-populated ">
           <section class="elementor-section elementor-inner-section elementor-element elementor-element-55f80f0 elementor-section-height-min-height elementor-section-boxed elementor-section-height-default" data-id="55f80f0" data-element_type="section" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
-            <div class="elementor-container elementor-column-gap-default justify-content-center">
+            <div class="elementor-container elementor-column-gap-default justify-content-center bg_thank">
 
-              <div class="elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-ecbd0fb" data-id="ecbd0fb" data-element_type="column">
-                <div class="elementor-widget-wrap elementor-element-populated">
+              <div class="elementor-column elementor-col-100 elementor-inner-column elementor-element elementor-element-ecbd0fb " data-id="ecbd0fb" data-element_type="column">
+                <div class="elementor-widget-wrap elementor-element-populated ">
                   <div class="elementor-element elementor-element-ce7f93e elementor-widget elementor-widget-heading" data-id="ce7f93e" data-element_type="widget" data-widget_type="heading.default">
                     <div class="elementor-widget-container">
-                      <h2 class="elementor-heading-title elementor-size-default">Thank you for <br>  Your Order! </h2>
+                      <h2 class="elementor-heading-title elementor-size-default text-center">Thank you for <br>  Your Order! </h2>
                     </div>
                   </div>
                   <div class="elementor-element elementor-element-0f249a1 elementor-widget elementor-widget-text-editor" data-id="0f249a1" data-element_type="widget" data-widget_type="text-editor.default">
                     <div class="elementor-widget-container">
-                      <p>A confirmation email was sent</p>
+                   
+                      <p class=" text-center" style="font-size: 20px;">A confirmation email was sent </p>
                     </div>
                   </div>
                 </div>
